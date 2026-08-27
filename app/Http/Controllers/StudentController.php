cb234b15950c9a6c38dbe2612d9bcca174a27cf2
Christian Joseph Aquino <christianjosephpaquino@gmail.com>
2026-08-27 21:19:30 +0800

    feat: create student controller

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    public function index()
+    {
+        $students = Student::latest()->get();
+
+        return view('students.index', compact('students'));
+    }
+
+    public function create()
+    {
+        return view('students.create');
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Student $student)
+    {
+        return view('students.show', compact('student'));
+    }
+}
